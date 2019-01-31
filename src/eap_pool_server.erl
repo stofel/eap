3ba44376d57483dbdeaa4db64093770516586f9c
Aleksey Kluchnikov <kluchnikovas@gmail.com>
2019-01-31 11:02:35 +0300

    fix exit conditions

--- src/eap_pool_server.erl
+++ src/eap_pool_server.erl
@@ -15,6 +15,7 @@
   checkout/2,
   checkin/2,
   get_worker/1,
+  start_worker/2,
   stat/1]).
 
 
@@ -45,11 +46,13 @@ code_change(_OldVersion, State, _Extra) ->
 %% Gen Server api
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% infos
-handle_info({'EXIT', Pid}, State)  -> somebody_exit_(State, Pid);
-handle_info(Msg, S)                -> ?INF("Unk msg:", Msg), {noreply, S}.
+handle_info({'EXIT', Pid}, State)           -> somebody_exit_(State, Pid);
+handle_info({'DOWN',_R,process,Pid,_Res},S) -> somebody_exit_(S, Pid);
+handle_info(Msg, S)                         -> ?INF("Unk msg:", Msg), {noreply, S}.
 %% casts                          
 handle_cast({checkout, Pid}, S)    -> checkout_(S, Pid);
 handle_cast({checkin,  Pid}, S)    -> checkin_(S, Pid);
+handle_cast({start_worker,Pid}, S) -> start_worker_(S, Pid);
 handle_cast(_Req, S)               -> ?INF("Unknown cast", _Req), {noreply, S}.
 %% calls                           
 handle_call(get_worker, _From, S)  -> get_worker_(S);
@@ -60,28 +63,34 @@ handle_call(_Req, _From, S)        -> {reply, ?e(unknown_command), S}.
 
 
 %%
-checkout(PoolName, Pid)          -> gen_server:cast(PoolName, {checkout, Pid}).
-checkout_(S = #{idle := I}, Pid) -> {noreply, S#{idle := lists:delete(Pid, I)}}.
-
-
-
-%%
-checkin(PoolName, Pid)          -> gen_server:cast(PoolName, {checkin, Pid}).
-checkin_(S = #{idle := I}, Pid) -> {noreply, S#{idle := lists:usort([Pid|I])}}.
+checkout(PoolName, Pid)           -> gen_server:cast(PoolName, {checkout, Pid}).
+checkout_(S = #{idle := I}, Pid)  -> {noreply, S#{idle := lists:delete(Pid, I)}}.
 
 
 %%
-get_worker(PoolName)                 -> gen_server:call(PoolName, get_worker).
-get_worker_(S = #{idle := [W|Rest]}) -> {reply, {ok, W}, S#{idle := Rest}};
-get_worker_(S = #{idle := []})       -> {reply, ?e(no_idle_worker_exists), S}.
+checkin(PoolName, Pid)            -> gen_server:cast(PoolName, {checkin, Pid}).
+checkin_(S = #{idle := I}, Pid)   -> {noreply, S#{idle := lists:usort([Pid|I])}}.
 
 
 %%
-stat(PoolName)                -> gen_server:call(PoolName, get_stat).
-stat_(S = #{idle := Workers}) -> {reply, #{idle_workers_num => length(Workers)}, S}.
+start_worker(PoolName, Pid)           -> gen_server:cast(PoolName, {start_worker, Pid}).
+start_worker_(S = #{idle := I}, Pid)  -> erlang:monitor(process, Pid), {noreply, S#{idle := lists:usort([Pid|I])}}.
+
+
+%%
+get_worker(PoolName)              -> gen_server:call(PoolName, get_worker).
+get_worker_(S = #{idle := [W|_]}) -> {reply, {ok, W}, S};
+get_worker_(S = #{idle := []})    -> {reply, ?e(no_idle_worker_exists), S}.
+
+
+%%
+stat(PoolName)                    -> gen_server:call(PoolName, get_stat).
+stat_(S = #{idle := Workers})     -> {reply, #{idle_workers_num => length(Workers)}, S}.
 
 
 %%
 somebody_exit_(S = #{idle := I}, Pid) ->
+  ?INF("AAAAA", Pid),
   {noreply, S#{idle := lists:delete(Pid, I)}}.
+
 
